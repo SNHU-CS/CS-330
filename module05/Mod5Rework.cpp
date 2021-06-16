d18226f4f85761aaed01a8ec8bde988b50ec8fad
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-06-15 20:25:51 -0500

    imports and namespace created

---
+++ module05/Mod5Rework.cpp
@@ -0,0 +1,106 @@
+// ***** IMPORTS/INCLUDE ******
+#include <iostream>         // cout, cerr
+#include <cstdlib>          // EXIT_FAILURE
+#include <GL/glew.h>        // GLEW library
+#include <GLFW/glfw3.h>     // GLFW library
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>      // Image loading Utility functions
+
+// GLM Math Header inclusions
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// imported classes
+#include <learnOpengl/camera.h> // Camera class
+
+using namespace std; // Standard namespace
+
+/*Shader program Macro*/
+#ifndef GLSL
+#define GLSL(Version, Source) "#version " #Version " core \n" #Source
+#endif
+
+// ***** NAMESPACE ******
+namespace
+{
+
+    const char* const WINDOW_TITLE = "Katie's Graphics Project"; // Macro for window title
+
+    // window width and height
+    const int WINDOW_WIDTH = 1200;
+    const int WINDOW_HEIGHT = 800;
+
+    // Stores the GL data relative to a given mesh
+    struct GLMesh
+    {
+        GLuint vao;         // Handle for the vertex array object
+        GLuint vbo;         // Handle for the vertex buffer object
+        GLuint nVertices;    // Number of indices of the mesh
+        // GLuint nIndices;    // Number of indices of the mesh
+    };
+
+    // Main GLFW window
+    GLFWwindow* gWindow = nullptr;
+    // Triangle mesh data
+    GLMesh gMesh;
+    // Texture
+    GLuint gTextureId;
+    glm::vec2 gUVScale(10.0f, 10.0f);
+    GLint gTexWrapMode = GL_REPEAT;
+
+    // Shader programs
+    GLuint gProgramId;
+    GLuint gCubeProgramId;
+    GLuint gLampProgramId;
+
+    // camera
+    Camera gCamera(glm::vec3(0.2f, 5.6f, 9.9f));
+    float gLastX = WINDOW_WIDTH / 2.0f;
+    float gLastY = WINDOW_HEIGHT / 2.0f;
+    bool gFirstMouse = true;
+
+    // frame rate (time between each frame)
+    float gDeltaTime = 0.0f; // time between current frame and last frame
+    float gLastFrame = 0.0f;
+
+    /*
+    // Subject position and scale
+    glm::vec3 gCubePosition(0.0f, 0.0f, 0.0f);
+    glm::vec3 gCubeScale(2.0f);
+
+    // Cube and light color
+    //m::vec3 gObjectColor(0.6f, 0.5f, 0.75f);
+    glm::vec3 gObjectColor(1.f, 0.2f, 0.0f);
+    glm::vec3 gLightColor(1.0f, 1.0f, 1.0f);
+
+    // Light position and scale
+    glm::vec3 gLightPosition(1.5f, 0.5f, 3.0f);
+    glm::vec3 gLightScale(0.3f);
+
+    // Lamp animation
+    bool gIsLampOrbiting = true;
+    */
+}
+
+/*Shader program Macro*/
+#ifndef GLSL
+#define GLSL(Version, Source) "#version " #Version " core \n" #Source
+#endif
+
+// ****** USER-DEFINED FUNCTIONS *****
+
+
+
+// shaders
+bool createShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId);
+void destroyShaderProgram(GLuint programId);
+
+
+
+
+
+
+
+
+
