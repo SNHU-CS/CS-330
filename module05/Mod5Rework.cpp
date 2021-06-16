153ef4011492daf0a2282ef05f7a5eb80602d8c9
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-06-16 15:03:35 -0500

    added generic texture creator/destroy. create ortho/perspective values. added initialize function with callbacks

--- module05/Mod5Rework.cpp
+++ module05/Mod5Rework.cpp
@@ -92,17 +92,17 @@ namespace
 // ****** USER-DEFINED FUNCTIONS *****
 // intialize OpenGL
 bool initializeOGL(int, char* [], GLFWwindow** window);
+//bool initializeWindow?Inout?Callback?(int, char* [], GLFWwindow** window);
 
 
-// camera
-
-
-// projection
-void changeProjection();
+// view and projection
+void resizeWindow(GLFWwindow* window, int width, int height);
 void toOrtho();
 void toPerspective();
+// use keyboard to change projection (event)
+void changeProjectionCallback();
 
-// navigation
+// user Inputs
 void mousePositionCallback(GLFWwindow* window, double xpos, double ypos);
 void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset);
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
@@ -110,19 +110,17 @@ void keyboardNavigation(GLFWwindow* window);
 void keyboardControl(GLFWwindow* window);
 
 // textures
-
-
-//
-
+void flipImageVertical(unsigned char* image, int width, int height, int channels);
+bool TextureSettings(const char* filename, GLuint& textureId);
+void destroyTexture(GLuint textureId);
 
 // shaders
 // generic shader
 bool createShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId);
 void destroyShaderProgram(GLuint programId);
-
+// unique shaders for lighting.
 // GLuint gCubeProgramId;
 // GLuint gLampProgramId;
-
 
 // draw objects
 void draw(floor);
@@ -182,14 +180,86 @@ void draw5();
 
 */
 
-// camera
 
+// ***** INTIALIZE OPENGL AND EVENTS *****
+// Initialize GLFW, GLEW, and user window
+bool initializeOGL(int argc, char* argv[], GLFWwindow** window)
+{
+    // GLFW: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // GLFW: window creation
+    // ---------------------
+    * window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if (*window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+    glfwMakeContextCurrent(*window);
+    glfwSetFramebufferSizeCallback(*window, resizeWindow);
+    glfwSetCursorPosCallback(*window, mousePositionCallback);
+    glfwSetScrollCallback(*window,  mouseScrollCallback);
+    glfwSetMouseButtonCallback(*window, mouseButtonCallback);
+    glfwSetProjectionCallback(*window, changeProjectionCallback);
+
+    // tell GLFW to capture our mouse
+    glfwSetInputMode(*window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // GLEW: initialize
+    // ----------------
+    // Note: if using GLEW version 1.13 or earlier
+    glewExperimental = GL_TRUE;
+    GLenum GlewInitResult = glewInit();
+
+    if (GLEW_OK != GlewInitResult)
+    {
+        std::cerr << glewGetErrorString(GlewInitResult) << std::endl;
+        return false;
+    }
+
+    // Displays GPU OpenGL version
+    cout << "INFO: OpenGL Version: " << glGetString(GL_VERSION) << endl;
+
+    return true;
+}
+
+
+// ***** view/camera - ADJUST THIS LATER*****
+// camera
 
 // adjusts window when screen size changed is changed (by OS or user)
 void resizeWindow(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
+
+// ***** PROJECTION SETTINGS *****
+void toOrtho()
+{
+    glm::mat4 projection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.1f, 100.0f);
+    GLint projLoc = glGetUniformLocation(gProgramId, "projection");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
+
+void toPerspective()
+{
+    glm::mat4 projection = glm::perspective(glm::radians(gCamera.Zoom), (GLfloat)WINDOW_WIDTH / (GLfloat)WINDOW_HEIGHT, 0.1f, 100.0f);
+    GLint projLoc = glGetUniformLocation(gProgramId, "projection");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+}
+
 
 // ***** INPUT DEVICES *****
 // input: mouse movement (inversed)
@@ -219,6 +289,7 @@ void mouseScrollCallback(GLFWwindow* window, double xoffset, double yoffset)
     // adjusts screen movement speed
     gCamera.ProcessMouseScroll(yoffset);
 }
+
 
 //  input: mouse button
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
@@ -262,6 +333,7 @@ void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
     }
 }
 
+
 //  input: keyboard navigation
 void keyboardNavigation(GLFWwindow* window)
 {
@@ -289,9 +361,14 @@ void keyboardNavigation(GLFWwindow* window)
         gCamera.ProcessKeyboard(DOWN, gDeltaTime);
 }
 
+
 //  input: keyboard controls (non-navigation)
 void keyboardControl(GLFWwindow* window) {
-    // add projection change here instead??
+    // add projection here. call on ortho
+    // use callback, not getkey
+        // source: https://www.glfw.org/docs/3.3/input_guide.html#input_key
+    // Creates a ortho projection
+    if (key == GLFW_KEY_P && action == GLFW_PRESS)
 }
 
 
@@ -304,6 +381,7 @@ void keyboardControl(GLFWwindow* window) {
 #ifndef GLSL
 #define GLSL(Version, Source) "#version " #Version " core \n" #Source
 #endif
+
 
 /* Vertex Shader Source Code*/
 // TODO: !!IMPORTANT MESSAGE AT START OF SHADER SECTION!!
@@ -351,7 +429,7 @@ const GLchar* fragmentShaderSource = GLSL(440,
 );
 
 
-// Implements the CreateShaders function
+// Implements the default Shader function
 //  TODO: !!IMPORTANT MESSAGE AT START OF SHADER SECTION!!
 //        update accordingly when shader program moves to shader.h
 bool createShaderProgram(const char* vtxShaderSource, const char* fragShaderSource, GLuint& programId)
@@ -422,6 +500,79 @@ void destroyShaderProgram(GLuint programId)
 {
     glDeleteProgram(programId);
 }
+
+
+// ***** TEXTURES AND IMAGES *****
+// flips images vertically to match OpenGl's y-axis
+// (the y-axis of OpenGL and standard images are inverted when compared).
+// CHECK ON HOW THIS AFFECTS THE DOOR, SUNSET PAINTING, AND POSSIBLY WREATH
+void flipImageVertical(unsigned char* image, int width, int height, int channels)
+{
+    for (int j = 0; j < height / 2; ++j)
+    {
+        int index1 = j * width * channels;
+        int index2 = (height - 1 - j) * width * channels;
+
+        for (int i = width * channels; i > 0; --i)
+        {
+            unsigned char tmp = image[index1];
+            image[index1] = image[index2];
+            image[index2] = tmp;
+            ++index1;
+            ++index2;
+        }
+    }
+}
+
+
+// generates settings for texture or image
+bool TextureSettings(const char* filename, GLuint& textureId)
+{
+    int width, height, channels;
+    unsigned char* image = stbi_load(filename, &width, &height, &channels, 0);
+    if (image)
+    {
+        flipImageVertically(image, width, height, channels);
+
+        glGenTextures(1, &textureId);
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
+        // set the texture wrapping parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        // set texture filtering parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        if (channels == 3)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+        else if (channels == 4)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+        else
+        {
+            cout << "Not implemented to handle image with " << channels << " channels" << endl;
+            return false;
+        }
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(image);
+        glBindTexture(GL_TEXTURE_2D, 0); // Unbind the texture
+
+        return true;
+    }
+
+    // Error loading the image
+    return false;
+}
+
+
+void destroyTexture(GLuint textureId)
+{
+    glGenTextures(1, &textureId);
+}
+
+
 
 
 
